9766d8896c145eb135f25c7d39b071a566304a63
uxbug <38550616+uxbug@users.noreply.github.com>
2024-01-28 17:59:39 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -4,7 +4,7 @@ adb wait-for-device
 adb -e push ramdisk.img /data/local/tmp/ramdisk.img.gz
 [[ -f magisk.zip ]] && adb -e push magisk.zip /data/local/tmp/magisk.zip
 [[ -f magisk.apk ]] && adb -e push magisk.apk /data/local/tmp/magisk.zip
-adb -e push busybox /data/local/tmp/
+adb -e push busybox_arm64 /data/local/tmp/
 adb -e push process.sh /data/local/tmp/
 adb -e shell "dos2unix /data/local/tmp/process.sh"
 adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp $1"
